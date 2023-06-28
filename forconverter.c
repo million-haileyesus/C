5d8fd5a46a64a2f848697d31c3eb949e6ebff49b
Million Haileyesus <noillimsey@gmail.com>
2023-06-27 22:22:38 -0400

    Create forconverter.c

---
+++ forconverter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define low 0
+#define high 320
+#define incr 20
+void main() {
+	for(int i = low; i <= high; i += incr) {
+		printf("%4d %6.1f\n", i, (5.0/9) * (i - 32));
+	}
+}
