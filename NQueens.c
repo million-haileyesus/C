2168b40552ed07919bbcd8b4d91d9099be10884d
Million Haileyesus <noillimsey@gmail.com>
2023-07-06 12:00:42 -0400

    Update NQueens.c

--- NQueens.c
+++ NQueens.c
@@ -7,7 +7,23 @@ void main() {
 	printf("%d", c);
 }
 
-bool isSafe() {
+void run() {
+	/**
+	 * int[num]* board;
+	 * bool can_quit = false;
+	 * for(int i = 1; i < board.length; i++) {
+	 * 		board[i] = -1;
+	 * 	}
+	 * 	if(board.length > 0) {
+	 * 		board[0] = 0;
+	 * 	}
+	 * 	for(int i = 0; i < board.length && !can_quit; i++) {
+	 *		if(is_safe()) {
+	 *		}
+	 *		else {
+	 *			while(
+	 */
+bool is_safe() {
 	int k = 1;
 	bool isSafe = true;
 	for(int i = 0; i < 10; i++) {
