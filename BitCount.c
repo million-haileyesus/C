9e48fdf1ccc31b5aa7239435f27ba8bc9c743953
Million Haileyesus <noillimsey@gmail.com>
2023-07-11 08:57:11 -0400

    Update BitCount.c

--- BitCount.c
+++ BitCount.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void main() {
-	int i;
-	int n = n;
-	for(i = 0; n != 0; n >>= 1) {
-		if(n & 01) {
-			i++;
-		}
-	}
-	printf("%d\n", i);
+    int i;
+    int n = n;
+    for(i = 0; n != 0; n >>= 1) {
+        if(n & 01) {
+            i++;
+        }
+    }
+    printf("%d\n", i);
 }
