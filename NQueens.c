c5bcba729d336c74dc1561f3dec0181dee46a4fa
Million Haileyesus <noillimsey@gmail.com>
2023-07-11 09:17:06 -0400

    Update NQueens.c

--- NQueens.c
+++ NQueens.c
@@ -3,44 +3,44 @@
 
 void main() {
     printf("%s", "choose a number:\n");
-	int input = getchar();
+    int input = getchar();
     int size, board_size;
-	size = board_size = 0;
+    size = board_size = 0;
     while(input != '\n') {
-		size = input - '0';
+        size = input - '0';
         board_size = 10 * board_size + size;
         input = getchar();
     }
-	printf("%d\n", board_size);	
+    printf("%d\n", board_size);
+	back_track();
 }
 
-void run() {
-    /**
-     * int[num]* board;
-     * bool can_quit = false;
-     * board[0] = 0;
-     * int size = sizeof(board) / sizeof(board[0]);
-     * for(int i = 1; i < size; i++) {
-     * 		board[i] = -1;
-     * 	}
-     *
-     * 	for(int i = 0; i < size && !can_quit; i++) {
-     *		if(is_safe()) {
-     *			if(i == size - 1) {
-     *				can_quit = true;
-     *				display();
-     *			}
-     *			else {
-     *				board[++i] = 0;
-     *			}
-     *		}
-     *		else {
-     *			while(i > 0 && board[i] == size - 1) {
-     *				board[i--] = -1;
-     *			}
-     *			can_quit = (++board[i] == size);
-     *
-     */
+void back_track(int board_size) {
+    bool can_quit = false;
+	int board[board_size];
+    board[0] = 0;
+    for(int i = 1; i < board_size; i++) {
+        board[i] = -1;
+    }
+
+    for(int i = 0; i < board_size && !can_quit; i++) {
+        if(is_safe()) {
+            if(i == board_size - 1) {
+                can_quit = true;
+                display();
+            }
+            else {
+                board[++i] = 0;
+            }
+        }
+        else {
+            while(i > 0 && board[i] == board_size - 1) {
+                board[i--] = -1;
+            }
+            can_quit = (++board[i] == board_size);
+        }
+    }
+
 }
 bool is_safe() {
     int k = 1;
