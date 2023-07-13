53a0c14d9dd1f3dbafe9f50305ed336a266e0c8e
Million Haileyesus <noillimsey@gmail.com>
2023-07-13 10:41:49 -0400

    Update wordcounting.c

--- wordcounting.c
+++ wordcounting.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 void main() {
-	int i, c;
-	for(i = 1; (c = getchar()) != '\n';) {
-		if(c == ' ') {
-			i++;
-		}
-	}
-	printf("%d\n", i);
+    int i, c;
+    for(i = 1; (c = getchar()) != '\n';) {
+        if(c == ' ') {
+            i++;
+        }
+    }
+    printf("%d\n", i);
 }
