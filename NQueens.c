1efeeb212a5a218df385ea5215a9dfd92e6e86bf
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-09-04 20:11:07 -0400

    Update NQueens.c

--- NQueens.c
+++ NQueens.c
@@ -73,7 +73,7 @@ bool is_viable(int board[], int i, int j, int board_size) {
         if(board[i - 1] == (j + k)) {
             is_safe = false;
         }
-        else if(board[i - 1] == (j - k)) {
+        if(board[i - 1] == (j - k)) {
             is_safe = false;
         }
     }
